33f6612849dee6c8f8fa3841629443cbf0a29e4a
S.m Mustakim Bellah <59763884+mustakimbellah@users.noreply.github.com>
2022-08-22 13:54:47 +0600

    budget-part-added

--- js/budget.js
+++ js/budget.js
@@ -1,1 +1,25 @@
-console.log('budget file added');
+
+document.getElementById('player-calculate-btn').addEventListener('click', function () {
+
+    const playerInputField = getInputFieldValueById('player-inputField');
+    const totalPlayerCost = playerInputField * 5;
+
+    setTextElementValueById('player-cost-total', totalPlayerCost);
+
+
+})
+
+document.getElementById('total-Cost-calculte-btn').addEventListener('click', function () {
+
+
+
+    const managetCost = getInputFieldValueById('managerInputField');
+
+    const coachCost = getInputFieldValueById('coachInputField');
+
+    const playerExpenses = getTextElementValueById('player-cost-total');
+
+    const totalCost = managetCost + coachCost + playerExpenses;
+
+    setTextElementValueById('total-cost-calculate', totalCost);
+})
--- js/common.js
+++ js/common.js
@@ -1,1 +1,23 @@
-console.log('common file added')
+//console.log('common file added')
+
+function getInputFieldValueById(inputId) {
+    const inputField = document.getElementById(inputId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseInt(inputFieldValueString);
+    inputField.value = '';
+    return inputFieldValue;
+
+}
+
+function getTextElementValueById(elementId) {
+    const textElement = document.getElementById(elementId);
+    const textElementValueString = textElement.innerText;
+    const textElementValue = parseInt(textElementValueString);
+    textElement.value = '';
+    return textElementValue;
+}
+
+function setTextElementValueById(elementId, newValue) {
+    const textElement = document.getElementById(elementId);
+    textElement.innerText = newValue;
+}
